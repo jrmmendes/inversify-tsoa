075dabd043d6dd25bb5fab7ee8b47ae00922700b
Romildo <jrmmendes@outlook.com>
2023-03-23 16:18:12 -0300

    build: add prebuild script

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Application } from "./application";
 
 const application = new Application();
 
+const port = <number><unknown>process.env.PORT ?? 3000;
+
 application.listen({
-  port: 3000,
+  port,
   hostname: 'localhost'
 });
